e3e7f72cfc2a4e9d2380fb1506cfb17b19b1b4e9
StefGeraets <stef.geraets@gmail.com>
2022-08-02 22:37:10 +0200

    Basic setup. Plus eslint, prettier and tailwind

---
+++ src/components/Map.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = unknown
+
+const Map = (props: Props) => {
+  return <div className='w-4/5'>Map</div>
+}
+
+export default Map
---
+++ src/components/Settings.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = unknown
+
+const Settings = (props: Props) => {
+  return <div className='w-1/5 bg-stone-100'>Settings</div>
+}
+
+export default Settings
