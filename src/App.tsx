fde203b290e26564ee5efa81e75d09ecb38b0b40
StefGeraets <stef.geraets@gmail.com>
2022-10-23 22:48:43 +0200

    Change grid rendering and clicking handling

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,13 @@ import Map from './components/Map'
 import Settings from './components/Settings'
 
 function App() {
+  const doAction = (e: any) => {
+    e.preventDefault();
+    console.log(e)
+  }
+
   return (
-    <div className='flex h-screen'>
+    <div className='flex h-screen' tabIndex={0} onKeyDown={doAction}>
       <Map />
       <Settings />
     </div>
--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,45 +1,50 @@
-import React from 'react'
-import { createArrayFromCellAmount } from '../lib/gridHelpers';
+import React, { CSSProperties, useState, MouseEvent } from 'react'
   
-export const renderGrid = (rows: number, cols: number) => {
-  const gridElements: JSX.Element[] = [];
-  let x = 0
-  let y = 0
-
-  createArrayFromCellAmount(rows, cols).map((c) => {
-    y = c % cols
-    
-    gridElements.push(GridCell(x, y, `${x}+${y}`))
-    if (y === rows - 1) {
-      x++
-    }
-  })
-
-  return gridElements;
-}
-
-const GridCell = (x: number, y: number, key?: string) => {
-  return (
-    <div className={`grid-item grid-item-${x}-${y} border border-gray-900/85`} key={key}></div>
-  )
-}
 
 type Props = {
-  rows: number, cols: number
+  rows: number, cols: number, cellSize?: number
 }
 
-const Grid = (props: Props) => {
+const Grid = ({rows = 24, cols = 24, cellSize = 32}: Props) => {
+  const [clickPosition, setClickPosition] = useState({left: 0, top: 0})
+
+  const setRingPosition = (e: MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    const clickPos = e.currentTarget.getBoundingClientRect();
+    const left = (e.clientX - clickPos.left);
+    const top = (e.clientY - clickPos.top);
+    const correctedLeft = left - (left % cellSize);
+    const correctedTop = top - (top % cellSize);
+    setClickPosition({left: correctedLeft, top: correctedTop})
+  }
+
+  const style = {
+    width: `${rows * cellSize}px`,
+    height: `${cols * cellSize}px`,
+    "--grid-cell-size": `${cellSize}px`
+  } as CSSProperties
+
   return (
     <div className={`
-      grid
-      grid-rows-[repeat(${props.rows},_32px)]
-      grid-cols-[repeat(${props.cols},_32px)]
+      block
       border
       border-gray-900/85
-    `}>
-      {renderGrid(props.rows, props.cols)}
+      map-grid
+      relative
+      focus-visible:outline-none
+    `}
+    style={style}
+    tabIndex={0} 
+    onClick={setRingPosition}
+    >
+      <div className='w-8 h-8 bg-red-600 absolute' style={clickPosition}></div>
     </div>
   )
 }
 
 export default Grid
+
+
+// Display grid with css
+// Add seperate element that moves accordingly to grid size
+// Set position of rings 
